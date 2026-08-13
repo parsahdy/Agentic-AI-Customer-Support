286323809facc47624bf83242c460048eda20bd1
unknown <pheydari33@yahoo.com>
2026-08-14 00:40:39 +0330

    extract chunker creation logic into factory

---
+++ knowledge_base/chunker_factory.py
@@ -0,0 +1,27 @@
+from chunker import (
+    ChunkerBase,
+    RecursiveCunker,
+    SentenceChunker,
+)
+
+
+class ChunkerFactory:
+
+    _chunkers: dict[str, type[ChunkerBase]] = {
+        "recursive": RecursiveCunker,
+        "sentence": SentenceChunker,
+    }
+
+    @classmethod
+    def create(cls, chunker: str) -> ChunkerBase:
+
+        chunker = chunker.lower().strip()
+
+        if not chunker in cls._chunkers:
+            raise ValueError(
+                f"Unkown chunker type: {chunker}"
+                f"Available chunkers: {list(cls._chunkers.keys())}"    
+            )
+
+        chunker_class = cls._chunkers[chunker]
+        return chunker_class()
