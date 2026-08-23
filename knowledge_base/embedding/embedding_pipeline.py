0a34ba711831d1b81eaccbe6cbd3480a2635b246
unknown <pheydari33@yahoo.com>
2026-08-23 03:39:10 +0330

    Update embedding pipeline

--- knowledge_base/embedding/embedding_pipeline.py
+++ knowledge_base/embedding/embedding_pipeline.py
@@ -3,16 +3,13 @@ from ..config import SENTENCE_EMBEDDING_MODEL
 
 
 def embedding_pipeline(documents: list[dict],
-                               embedding_type: str = "sentence",
-                               model_name: str = SENTENCE_EMBEDDING_MODEL):
+                       embedding_type: str = "sentence",
+                       model_name: str = SENTENCE_EMBEDDING_MODEL):
 
     embedder = EmbeddingFactory.create(
         embedding_type=embedding_type,
         model_name=model_name
     )
-
     documents_embeddings = embedder.embed(documents)
 
     return documents_embeddings
-
-    
