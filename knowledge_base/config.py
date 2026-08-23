5cac2ab6f7b863c6813ee607371c9c349d126540
unknown <pheydari33@yahoo.com>
2026-08-23 18:46:57 +0330

    chore: update config file

--- knowledge_base/config.py
+++ knowledge_base/config.py
@@ -1,15 +1,22 @@
 from pathlib import Path
 
+#3 Chnking
 CHUNKER = {
     "chunk_size": 1000,
     "chunk_overlap": 200,
 }
 
-K = 5
-
+## Embedding
 SENTENCE_EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 BGE_MODEL = "BAAI/bge-m3"
 
+## retriever
+K = 5
+K = 5
+SCORE = int
+THRESHOLD = int
+
+## Directories
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 KB_DIR = PROJECT_ROOT / "data" / "knowledge_base" 
 INDEX_PATH = KB_DIR / "vector_index.faiss"
