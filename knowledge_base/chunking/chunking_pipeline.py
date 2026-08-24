72a7356f23f0f06ab3d643b8de8e2f13f20740ec
unknown <pheydari33@yahoo.com>
2026-08-24 13:17:02 +0330

    fixes and updates

--- knowledge_base/chunking/chunking_pipeline.py
+++ knowledge_base/chunking/chunking_pipeline.py
@@ -2,12 +2,12 @@ from .chunker_factory import ChunkerFactory
 
 
 def chunk_pipeline(documents: list[dict], 
-                   chunker: str = "recursive") -> list[dict]:
+                   chunker_type: str = "recursive") -> list[dict]:
 
     if not documents:
         return []
 
-    chunker = ChunkerFactory.create(chunker)
+    chunker = ChunkerFactory.create(chunker_type)
 
     chunked_documents = chunker.chunk(documents)
 
